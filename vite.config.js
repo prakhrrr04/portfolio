ec1a06f064dea247fdbbba7c8ac9acde58580488
Prakhar Singh <prakharnsingh@Prakharrrs-MacBook-Air.local>
2025-06-30 04:19:48 +0530

    Fix: Repository Name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/prakhrrr04/',
+  base: '/portfolio/',
   plugins: [react()],
 });
